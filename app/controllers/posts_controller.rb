84d140654c8024dd2754ad0c80169e1086384f00
maoslele <m01.sgwk@gmail.com>
2020-10-17 01:11:52 +0900

    add: リスト追加アクションを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,12 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  def new
+
+  end
+  def create
+    @post = Post.new(title: params[:title], content:params[:content])
+    @post.save
+    redirect_to("/posts/index")
+  end
 end
