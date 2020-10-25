23fd73743fca813d69716469a3585e11b41892d6
maoslele <m01.sgwk@gmail.com>
2020-10-25 22:45:49 +0900

    add: Postsテーブルにcategoryカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_24_111531) do
+ActiveRecord::Schema.define(version: 2020_10_25_134348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_10_24_111531) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "content"
     t.integer "user_id"
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
